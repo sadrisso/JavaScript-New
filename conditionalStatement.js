f45e98064c088544fce38987c4a8d64069394d75
sadrisso <sadrisso2000@gmail.com>
2024-04-09 00:33:51 +0600

    conditional statement

---
+++ conditionalStatement.js
@@ -0,0 +1,57 @@
+
+
+const Age = 22;
+if(Age > 18){
+    console.log('Adult')
+}else{
+    console.log('Child')
+}
+
+let color = 'black';
+if(color == 'black'){
+    console.log('dark mode enable')
+}else{
+    console.log('light mode enable')
+}
+
+
+const age = 20;
+
+if(age <= 10){
+    console.log('Child')
+}else if(age > 10 && age < 18){
+    console.log('Not child')
+}else if(age > 18){
+    console.log('Adult')
+}
+
+const mark = 50;
+
+if(mark == 33){
+    console.log('Passed')
+}else if(mark > 33 && mark <= 40){
+    console.log('d')
+}else if(mark > 40 && mark <= 50){
+    console.log('b')
+}else if(mark > 50 && mark <= 60){
+    console.log('a-')
+}else if(mark > 60 && mark <= 80){
+    console.log('a')
+}else{
+    console.log('a+')
+}
+
+
+let number = 5;
+if(number % 2 == 0){
+    console.log('even number')
+}else{
+    console.log('odd number')
+}
+
+if(number % 3 == 0 || number % 1 == 0){
+    console.log('Divisiable by 3 or 1')
+}else{
+    console.log('not divisible by 3 or 1')
+}
+    

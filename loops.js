49f1b39bfae20cb2cb87d6b2d350ed82ab790026
sadrisso <sadrisso2000@gmail.com>
2024-04-09 02:15:11 +0600

    while and do while loop

--- loops.js
+++ loops.js
@@ -25,3 +25,30 @@ for (y = 0; y <= n; y+=2){
     summ += y
 }
 console.log(summ)
+
+
+// while loop
+
+let hello = 10;
+
+while(hello <= 12){
+    console.log('hello')
+    hello ++;
+}
+
+let ssum = 0;
+let m = 10;
+let l = 20;
+
+while(m <= l){
+    ssum += m
+    m++
+}
+console.log(ssum)
+
+
+let a = 0
+do {
+    console.log(a)
+    a++
+}while(a >= 10)

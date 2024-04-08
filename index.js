df950a1681535c57edb23a59490514ddcf23a946
sadrisso <sadrisso2000@gmail.com>
2024-04-08 20:58:08 +0600

    decliaring variable

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+
 console.log('Hi')
+
+// string type variable
+fullName = 'Shoeb akter drisso';
+console.log(fulName)
+
+surName = 'Drisso';
+console.log(surName)
+
+// integer data type variable
+number = 100;
+console.log(number)
+
+
+// float data type variable
+floatValue = 3.17
+console.log(floatValue)
+
+
+// boolean type variable
+boll = true
+console.log(boll)

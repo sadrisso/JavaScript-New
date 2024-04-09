93e558a560fff8c370c3702b654acfeba9987989
sadrisso <sadrisso2000@gmail.com>
2024-04-09 15:16:26 +0600

    template literals

---
+++ templateLiteral.js
@@ -0,0 +1,12 @@
+
+let product = {
+    name : 'pen',
+    price : 10
+}
+
+console.log(`The price of the ${product.name} is ${product.price} dbt`)
+console.log('Price of the', product.name, 'is', product.price, 'bdt')
+
+
+let exm = `This is a template literal ${4*4}`
+console.log(exm)

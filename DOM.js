0c80b25db26c2fb7575e3d2f34df7c77a7a67156
sadrisso <sadrisso2000@gmail.com>
2024-04-16 23:33:54 +0600

    basic DOM selection

---
+++ DOM.js
@@ -0,0 +1,24 @@
+console.dir(window.document)
+console.log(document.body)
+
+let headingOne = document.getElementById('headingOne');
+console.log(headingOne)
+
+let headingFour = document.getElementsByClassName('headingdFour');
+console.log(headingFour)
+
+let pera = document.getElementsByTagName('p');
+console.dir(pera)
+
+
+let firstEl = document.querySelector('h1');
+console.dir(firstEl)
+
+let allEle = document.querySelectorAll('h1');
+console.dir(allEle)
+
+let firstPera = document.querySelector('.pera')
+console.log(firstPera)
+
+let input = document.querySelector('#input')
+console.log(input)

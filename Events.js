8c20048b90b57c0582dfcc2e4217f760c5fde1c2
sadrisso <sadrisso2000@gmail.com>
2024-04-18 14:37:30 +0600

    event listener add and remove

--- Events.js
+++ Events.js
@@ -1,13 +1,36 @@
 let div = document.querySelector('div');
-div.onmouseover = () => {
-    console.log('You are inside the div..');
-}
+// div.onmouseover = () => {
+//     console.log('You are inside the div..');
+// }
+
+div.addEventListener('mouseover', (evt) => {
+    console.log(evt);
+    console.log('inside div');
+});
 
 let btn = document.querySelector('#btn1');
-btn.onclick = (evt) => {
-    console.log(evt)
-    console.log(evt.type)
-    console.log(evt.target)
-    console.log(evt.clientX, evt.clientY)
-    console.log('Clicked');
-}
+// btn.onclick = (evt) => {
+//     console.log(evt)
+//     console.log(evt.type)
+//     console.log(evt.target)
+//     console.log(evt.clientX, evt.clientY)
+//     console.log('Clicked');
+// }
+
+
+let clicked2 = () => {
+    console.log('Clicked 2');
+};
+
+btn.addEventListener('click', (evt) => {
+    console.log(evt);
+    console.log('Clicked 1');
+});
+
+btn.addEventListener('click', clicked2);
+btn.removeEventListener('click', clicked2);
+
+btn.addEventListener('click', (evt) => {
+    console.log(evt);
+    console.log('Clicked 3');
+});

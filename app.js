e667557aa40db167794fbe1a15855fff2c63057e
sadrisso <sadrisso2000@gmail.com>
2024-04-19 17:51:35 +0600

    generate users choice

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ let compScore = 0;
 const choices = document.querySelectorAll('.choice');
 choices.forEach((choice) => {
     choice.addEventListener('click', () => {
-        console.log('Choice was clicked');
+        let userChoice = choice.getAttribute('id');
+        playGame(userChoice)
     });
 })
+
+
+const playGame = (userChoice) => {
+    console.log('User choice =',userChoice)
+}
+ 

75559583b2e706e2aa99fee6d0342451999d29a9
sadrisso <sadrisso2000@gmail.com>
2024-04-21 19:56:40 +0600

    generate computer choice

--- new.js
+++ new.js
@@ -6,3 +6,9 @@ choices.forEach((choice) => {
         console.log(`User choice is: ${userChoice}`)
     });
 });
+
+const genCompChoice = () => {
+    let options = ['rock', 'paper', 'scissor'];
+    let idx = Math.floor(Math.random() * 3);
+    return options[idx];
+}

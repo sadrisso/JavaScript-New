5bd45d4e09d0037109a9986150e4170804775c8f
sadrisso <sadrisso2000@gmail.com>
2024-04-21 20:20:13 +0600

    play game logic

--- new.js
+++ new.js
@@ -4,8 +4,30 @@ choices.forEach((choice) => {
     choice.addEventListener('click', () => {
         let userChoice = choice.getAttribute('id')
         console.log(`User choice is: ${userChoice}`)
+        playGame(userChoice)
     });
 });
+
+
+const playGame = (userChoice) => {
+    console.log(`User choice is ${userChoice}`);
+    let compChoice = genCompChoice();
+    console.log(`Comp choice is ${compChoice}`);
+
+    if (userChoice === compChoice) {
+        console.log('Draw');
+    } else {
+        let userVal = true;
+        if (userChoice === 'rock') {
+            userVal = compChoice === 'paper' ? false : true;
+        } else if (userChoice === 'paper') {
+            userVal = compChoice === 'scissor' ? false : true;
+        } else {
+            userVal = compChoice === 'rock' ? false : true;
+        }
+
+    }
+}
 
 const genCompChoice = () => {
     let options = ['rock', 'paper', 'scissor'];
